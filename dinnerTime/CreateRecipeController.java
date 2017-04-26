d2e7324e0ab8c44171f08d0f0087eaaad581cea8
Olof Nymanson <onymanson@gmail.com>
2017-04-26 11:18:02 +0200

    + alla länder i en combobox

--- dinnerTime/CreateRecipeController.java
+++ dinnerTime/CreateRecipeController.java
@@ -1,14 +1,21 @@
 package dinnerTime;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
-public class CreateRecipeController implements Initializable{
+public class CreateRecipeController implements Initializable {
 	@FXML
 	private Button btnSend, btnAddIngredient;
 	@FXML
@@ -16,25 +23,36 @@ public class CreateRecipeController implements Initializable{
 	@FXML
 	private TextArea taIngredients, taInstruction;
 	@FXML
-	private Label lblConfirmation; 
-	
+	private Label lblConfirmation;
+	@FXML
+	private ComboBox cbCountry;
+
 	private Client client;
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		taIngredients.setEditable(false);
 		lblConfirmation.setVisible(false);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/Countries.txt"));
+			String strLine = br.readLine();
+			while (strLine != null) {
+				cbCountry.getItems().add(strLine);
+				strLine = br.readLine();
+			}
+		} catch (IOException e) {
+		}
 	}
 
 	@FXML
-	private void sendRecipe(){
+	private void sendRecipe() {
 		Recipe recipe = new Recipe();
-		
+
 		if (!tfTitle.getText().isEmpty()) {
 			recipe.setTitle(tfTitle.getText());
 		}
-		if (!tfCountry.getText().isEmpty()) {
-			recipe.setCountry(tfCountry.getText());
+		if(cbCountry.getValue() != null){
+			recipe.setCountry(cbCountry.getValue().toString());
 		}
 		if (!tfTime.getText().isEmpty()) {
 			recipe.setTime(Integer.parseInt(tfTime.getText()));
@@ -45,26 +63,25 @@ public class CreateRecipeController implements Initializable{
 				recipe.setIngredient(ingredients[i]);
 			}
 		}
-		if(!taInstruction.getText().isEmpty()){
+		if (!taInstruction.getText().isEmpty()) {
 			recipe.setInstruction(taInstruction.getText());
 		}
 		recipe.setAuthor(client.getUsername());
 		client.sendToServer(recipe);
 		lblConfirmation.setVisible(true);
 	}
-	
+
 	@FXML
-	private void addIngredient(){
-		if(taIngredients.getText().isEmpty()){
+	private void addIngredient() {
+		if (taIngredients.getText().isEmpty()) {
 			taIngredients.setText(tfIngredientInput.getText());
-		}
-		else{
+		} else {
 			taIngredients.setText(taIngredients.getText() + "\n" + tfIngredientInput.getText());
 		}
 		tfIngredientInput.setText("");
-	} 
-	
-	public void setClient(Client client){
+	}
+
+	public void setClient(Client client) {
 		this.client = client;
 	}
 }
