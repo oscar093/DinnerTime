85a61d355a22e3281c1c06f3ffd07ba0dec18cd6
OlofNymanson <onymanson@gmail.com>
2017-05-01 22:11:36 +0200

    Inga ändringar

---
+++ dinnerTime/Client.java
@@ -0,0 +1,139 @@
+package dinnerTime;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Client extends Thread {
+	private String ip;
+	private int port;
+	private Socket socket;
+	private ObjectOutputStream oos;
+	private ObjectInputStream ois;
+	private Runnable onConnected;
+	private LoginViewController lvc;
+	private RegisterViewController rvc;
+	private ClientViewController cvm;
+	
+	private ArrayList<Recipe> downloadedRecipes = new ArrayList<Recipe>();
+	private String username;
+
+	
+	public void setOnConnected(Runnable onConnected) {
+		this.onConnected = onConnected;
+	}
+
+	public Client(String ip, int port, LoginViewController lvc) {
+		this.ip = ip;
+		this.port = port;
+		this.lvc = lvc;
+	}
+	
+	public Client(String ip, int port, RegisterViewController rvc) {
+		this.ip = ip;
+		this.port = port;
+		this.rvc = rvc;
+	}
+	
+	public void setClientViewController(ClientViewController cvm){
+		this.cvm= cvm;
+	}
+
+	public void run() {
+		try {
+			socket = new Socket(ip, port);
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			ois = new ObjectInputStream(socket.getInputStream());
+			if(onConnected != null) {
+				onConnected.run();
+			}
+			while (true) {
+				try {
+					Object obj = ois.readObject();
+					if (obj instanceof Login) {
+						String loginStatus = ((Login) obj).getLoginStatus();
+						lvc.setLoginStatus(loginStatus);
+					}
+					else if(obj instanceof Register) {
+						String registerStatus = ((Register) obj).getRegisterStatus();
+						rvc.setRegisterStatus(registerStatus);
+					}
+					else if(obj instanceof Recipe){
+						
+					}else if(obj instanceof Recipe[]){
+						storeRecipes((Recipe[]) obj);
+					}
+				} catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+
+	/**
+	 * Sends object to server. Make Sure Object is instance of known type.
+	 * @param obj
+	 */
+	public void sendToServer(Object obj) {
+		try {
+			if(obj instanceof Recipe){
+				Recipe r = (Recipe)obj;
+				System.out.println(r.getTitle() + " " + r.getAuthor());
+			}
+			oos.writeObject(obj);
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Find recipe by title.
+	 * @param title
+	 * @return recipe
+	 */
+	public Recipe getRecipe(String title){
+		
+		for(Recipe r : downloadedRecipes){
+			if(r.getTitle().toLowerCase().contentEquals(title.toLowerCase())){
+				return r;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Find recipe by id.
+	 * @param title
+	 * @return recipe
+	 */
+	public Recipe getRecipe(int id){
+		
+		for(Recipe r : downloadedRecipes){
+			if(r.getId() == id){
+				return r;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Stores all downloaded recipes.
+	 * @param array of recipes
+	 */
+	public void storeRecipes(Recipe[] recipes){
+		cvm.updateCountryNode(recipes);
+		for(Recipe r : recipes){
+			downloadedRecipes.add(r);
+		}
+	}
+}
---
+++ dinnerTime/LoginViewController.java
@@ -0,0 +1,94 @@
+package dinnerTime;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginViewController implements Initializable {
+	@FXML
+	private Main main;
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private Button login;
+	@FXML
+	private Button register;
+	
+	private Client client;
+	
+	private Login userLogin;
+	
+	private String loginStatus;
+	
+	@FXML
+	private Label loginInfo;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		loginInfo.setVisible(false);
+	}
+	
+	@FXML
+	public void login() throws IOException {
+		loginStatus = "timeout";
+		loginInfo.setVisible(false);
+		
+		//Mot remote server
+//		client = new Client("146.148.4.203", 3250, this);
+		
+		// Mot lokal server
+		client = new Client("localhost", 3250, this);
+		
+		userLogin = new Login(username.getText(), password.getText());
+		client.setOnConnected(() -> client.sendToServer(userLogin));
+		client.start();
+		int waitForLoginStatusCounter = 0;
+		while(loginStatus.equals("timeout")) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			waitForLoginStatusCounter++;
+			if(waitForLoginStatusCounter >= 20) {
+				break;
+			}
+		}
+		if(loginStatus.equals("success")) {
+			client.setUsername(username.getText());
+			main.showClientView(client);
+		}
+		else if(loginStatus.equals("failed")) {
+			loginInfo.setVisible(true);
+			loginInfo.setText("Login failed: Wrong username or password");
+		}
+		else {
+			loginInfo.setVisible(true);
+			loginInfo.setText("Timeout: Check connection");
+		}
+	}
+	
+	@FXML
+	public void register() throws IOException {
+		main.showRegisterView();
+	}
+	
+	public String getLoginStatus() {
+		return loginStatus;
+	}
+	
+	public void setLoginStatus(String loginStatus) {
+		this.loginStatus = loginStatus;
+	}
+}
